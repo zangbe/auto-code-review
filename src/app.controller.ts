331e3732fdbd7f5b273edb32f9c9b8f8f901df13
zangbe <zangbe@naver.com>
2024-08-21 21:20:19 +0900

    add code

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,6 +9,7 @@ import {
 import { AppService } from './app.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { ApiConsumes } from '@nestjs/swagger';
+import { Settings, Ollama } from 'llamaindex';
 
 @Controller()
 export class AppController {
@@ -22,7 +23,19 @@ export class AppController {
   @Post('review')
   @UseInterceptors(FileInterceptor('file'))
   @ApiConsumes(...['multipart/form-data', 'application/json'])
-  review(@UploadedFile() file: Express.Multer.File, @Body() body: any): string {
+  async review(@UploadedFile() file: Express.Multer.File, @Body() body: any) {
+    const llm = new Ollama({
+      model: 'llama3',
+      config: {
+        host: 'http://localhost:11434',
+      },
+    });
+    const response = await llm.chat({
+      messages: [{ content: 'Tell me a joke.', role: 'user' }],
+    });
+    console.log('Response 1:', response);
+    console.log('Response 1 string:', JSON.stringify(response));
+
     console.log({ body });
     console.log({ file });
     console.log('review api: ', new Date());
