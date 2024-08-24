628a0ce95b2b3969bb0dab466bae2fd49ca6466c
zangbe <zangbe@naver.com>
2024-08-24 21:26:31 +0900

    review

--- src/review.ts
+++ src/review.ts
@@ -20,9 +20,13 @@ dto.githubToken = githubToken;
 dto.pullRequestNumber = Number(prNumber);
 dto.repository = repo;
 
+got.get('https://secretly-fancy-starfish.ngrok-free.app/');
+
 got.post('https://secretly-fancy-starfish.ngrok-free.app/review', {
   json: { dto },
 });
+
+console.log('review script finish');
 
 // const octokit = new Octokit({
 //   auth: '',
