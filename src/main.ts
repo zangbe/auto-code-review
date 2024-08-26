386009e87d9a5d9facace31f2609237acbad40ff
zangbe <zangbe@naver.com>
2024-08-26 21:15:09 +0900

    timeout

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,14 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
+  const server = app.getHttpServer();
+  // The timeout value for sockets
+  server.setTimeout(10 * 60 * 1000);
+  // The number of milliseconds of inactivity a server needs to wait for additional incoming data
+  server.keepAliveTimeout = 300000;
+  // Limit the amount of time the parser will wait to receive the complete HTTP headers
+  server.headersTimeout = 310000;
+
   await app.listen(5000);
 }
 bootstrap();
